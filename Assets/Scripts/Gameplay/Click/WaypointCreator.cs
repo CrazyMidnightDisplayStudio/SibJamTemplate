a7b6e34e1ee9964b19983bbb7e2b1c7926acf0b2
Vdovenko Sergey <39949940+DeFauste@users.noreply.github.com>
2024-11-03 20:17:36 +0300

    fix: удаление избыточных точек

--- Assets/Scripts/Gameplay/Click/WaypointCreator.cs
+++ Assets/Scripts/Gameplay/Click/WaypointCreator.cs
@@ -1,10 +1,12 @@
+using NUnit.Framework;
+using System.Collections.Generic;
 using UnityEngine;
 using UnityEngine.Events;
 
 public class WaypointCreator : MonoBehaviour
 {
     [SerializeField] private GameObject pointPrefab;
-
+    GameObject _lastClick;
 	HumanController humanController;
 
 	void Awake()
@@ -18,6 +20,15 @@ public class WaypointCreator : MonoBehaviour
         mousePosition.z = 0;
 
         GameObject point = Instantiate(pointPrefab, mousePosition, Quaternion.identity);
+        DeletePrevPoint(point);
         return point.transform;
+    }
+    private void DeletePrevPoint(GameObject newPoint)
+    {
+       if(_lastClick != null) 
+        { 
+            Destroy(_lastClick); _lastClick = null; 
+        } 
+        _lastClick = newPoint;
     }
 }
