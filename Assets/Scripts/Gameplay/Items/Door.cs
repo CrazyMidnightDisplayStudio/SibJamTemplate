e4e4ca0a74ee588c703f8ad2af5f6bd544e9392a
Erosrolf <Erosrolf@gmail.com>
2024-11-03 18:58:08 +0300

    added DoorsService

--- Assets/Scripts/Gameplay/Items/Door.cs
+++ Assets/Scripts/Gameplay/Items/Door.cs
@@ -1,31 +1,55 @@
 using UnityEngine;
 
-public class Door : MonoBehaviour
+namespace Gameplay.Items
 {
-    private Collider2D doorCollider;
-    private Animator animator;
-
-    void Start()
+    public class Door : MonoBehaviour
     {
-        doorCollider = transform.parent.GetComponent<Collider2D>();
-        animator = transform.parent.GetComponent<Animator>();
-    }
-
-    void OnTriggerEnter2D(Collider2D other)
-    {
-        if (other.CompareTag("Player") || other.CompareTag("Human"))
+        [SerializeField] private int _doorID;
+        private Collider2D doorCollider;
+        private Animator animator;
+        
+        public bool IsOpen { get; private set; }
+        public bool IsLocked { get; private set; }
+        public int DoorID => _doorID;
+    
+        private void Awake()
         {
-            doorCollider.enabled = false;
-            animator.SetTrigger("Open");
+            doorCollider = transform.parent.GetComponent<Collider2D>();
+            animator = transform.parent.GetComponent<Animator>();
         }
-    }
+        
+        public void Lock() => IsLocked = true;
+        public void Unlock() => IsLocked = false;
 
-    void OnTriggerExit2D(Collider2D other)
-    {
-        if (other.CompareTag("Player") || other.CompareTag("Human"))
+        public void OpenDoor()
         {
-            doorCollider.enabled = true;
+            if (IsLocked) return;
+            animator.SetTrigger("Open");
+            doorCollider.enabled = false;
+            IsOpen = true;
+        }
+
+        public void CloseDoor()
+        {
             animator.SetTrigger("Close");
+            doorCollider.enabled = true;
+            IsOpen = false;
+        }
+
+        private void OnTriggerEnter2D(Collider2D other)
+        {
+            if (other.CompareTag("Player") || other.CompareTag("Human"))
+            {
+                OpenDoor();
+            }
+        }
+
+        private void OnTriggerExit2D(Collider2D other)
+        {
+            if (other.CompareTag("Player") || other.CompareTag("Human"))
+            {
+                CloseDoor();
+            }
         }
     }
 }
---
+++ Assets/Scripts/Gameplay/Services/DoorsService.cs
@@ -0,0 +1,64 @@
+﻿using System.Collections.Generic;
+using Gameplay.Items;
+using UnityEngine;
+
+namespace Game.Services.Debugging.Gameplay.Services
+{
+    public class DoorsService
+    {
+        private Dictionary<int, Door> _doorsMap = new Dictionary<int, Door>();
+
+        public DoorsService()
+        {
+            GameObject doors = GameObject.Find("Doors");
+            if (doors == null)
+            {
+                Debug.LogError("Doors not found on scene");
+                return;
+            }
+
+            var doorsComponents = doors.GetComponentsInChildren<Door>();
+            foreach (var door in doorsComponents)
+            {
+                _doorsMap.Add(door.DoorID, door);
+            }
+
+            if (_doorsMap.Count != doorsComponents.Length)
+            {
+                Debug.LogError("Non unique doors id");
+            }
+        }
+
+        public void LockDoors(int[] doorIDs)
+        {
+            foreach (var id in doorIDs)
+            {
+                _doorsMap[id].Lock();
+            }
+        }
+
+        public void UnlockDoors(int[] doorIDs)
+        {
+            foreach (var id in doorIDs)
+            {
+                _doorsMap[id].Unlock();
+            }
+        }
+
+        public void OpenDoors(int[] doorIDs)
+        {
+            foreach (var id in doorIDs)
+            {
+                _doorsMap[id].OpenDoor();
+            }
+        }
+
+        public void CloseDoors(int[] doorIDs)
+        {
+            foreach (var id in doorIDs)
+            {
+                _doorsMap[id].CloseDoor();
+            }
+        }
+    }
+}
