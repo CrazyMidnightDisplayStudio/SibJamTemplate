850886f11ebab53400950ed2541ffa5f9d850af2
Vdovenko Sergey <39949940+DeFauste@users.noreply.github.com>
2024-11-04 13:09:12 +0300

    add: audio settings menu

---
+++ Assets/Scripts/Gameplay/Audio/AudioManager.cs
@@ -0,0 +1,31 @@
+﻿using Core.Audio;
+using UnityEngine;
+using Zenject;
+
+namespace Assets.Scripts.Gameplay.Audio
+{
+    public class AudioManager : MonoBehaviour
+    {
+        private AudioService _audioService;
+
+        [Inject]
+        public void Construct(AudioService audioSource)
+        {
+            _audioService = audioSource;
+        }
+
+
+        public void Master(float value)
+        {
+            _audioService.SetMasterVolume(value);
+        }
+        public void Music(float value)
+        {
+            _audioService.SetMusicVolume(value);
+        }
+        public void SFX(float value)
+        {
+            _audioService.SetSfxVolume(value);
+        }
+    }
+}
