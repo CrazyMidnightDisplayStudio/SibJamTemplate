03c578ebe0eb9572f97e692dd5501bbb448903e4
Vdovenko Sergey <39949940+DeFauste@users.noreply.github.com>
2024-11-07 15:06:55 +0300

    add: FullScreen

---
+++ Assets/Scripts/MainMenu/GameSettings.cs
@@ -0,0 +1,35 @@
+﻿using Core.Audio;
+using UnityEngine;
+using Zenject;
+
+namespace Assets.Scripts.MainMenu
+{
+    public class GameSettings: MonoBehaviour
+    {
+        private AudioService _audioService;
+
+        [Inject]
+        public void Construct(AudioService audioSource)
+        {
+            _audioService = audioSource;
+        }
+        #region Audio
+        public void Master(float value)
+        {
+            _audioService.SetMasterVolume(value);
+        }
+        public void Music(float value)
+        {
+            _audioService.SetMusicVolume(value);
+        }
+        public void SFX(float value)
+        {
+            _audioService.SetSfxVolume(value);
+        }
+        #endregion
+
+        #region Screen
+        public void IsFullScreen(bool value) => Screen.fullScreen = value;
+        #endregion
+    }
+}
